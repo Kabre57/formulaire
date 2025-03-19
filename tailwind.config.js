a140b96555210896713cddfeb45473bdd70e15e8
Geoffroy Theo <130750391+Kabre57@users.noreply.github.com>
2025-03-19 23:57:08 +0000

    commit 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}', // Assurez-vous que ce chemin est correct
+    ],
     theme: {
         extend: {},
     },
